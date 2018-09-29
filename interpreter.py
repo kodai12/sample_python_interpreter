7d9f7dd4e8ecbd0dfa153e32616c72fe727c1ca3
sakochik <sakochi.kodai1209@gmail.com>
2018-09-29 23:26:59 +0900

    add: interpreter => run_code method

--- interpreter.py
+++ interpreter.py
@@ -9,8 +9,21 @@ class Interpreter():
     answer = self.stack.pop()
     print(answer)
 
-  def ADD_TWO_VALUE(self):
+  def ADD_TWO_VALUES(self):
     first = self.stack.pop()
     second = self.stack.pop()
     total = first + second
     self.stack.append(total)
+
+  def run_code(self, what_to_excute):
+    instructions = what_to_excute["instructions"]
+    numbers = what_to_excute["numbers"]
+    for each_step in instructions:
+      instruction, argument = each_step
+      if instruction == "LOAD_VALUE":
+        number = numbers[argument]
+        self.LOAD_VALUE(number)
+      elif instruction == "ADD_TWO_VALUES":
+        self.ADD_TWO_VALUES()
+      elif instruction == "PRINT_ANSWER":
+        self.PRINT_ANSWER()
