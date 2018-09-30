5657182cb93a910afc0cf6520a028c3d28b34bf0
sakochik <sakochi.kodai1209@gmail.com>
2018-09-30 19:58:19 +0900

    add: create crud method about frame in virtual machine

--- virtual_machine.py
+++ virtual_machine.py
@@ -12,3 +12,35 @@ class VirtualMachine(object):
     """ An entry point to excute code using the virtual machine """
     frame = self.make_frame(code, global_names=global_names, local_names=local_names)
     self.run_frame(frame)
+
+  # frame manipulation
+  def make_frame(self, code, callargs={}, global_names=None, local_names=None):
+    if global_names is not None and local_names is not None:
+      local_names = global_names
+    elif self.frames:
+      global_names = self.frame.global_names
+      local_names = {}
+    else:
+      global_names = local_names = {
+        '__builtins__': __builtins__,
+        '__name__': '__main__',
+        '__doc__': None,
+        '__package__': None,
+      }
+    local_names.update(callargs)
+    frame = Frame(code, global_names, local_names, self.frame)
+    return frame
+
+  def push_frame(self, frame):
+    self.frames.append(frame)
+    self.frame = frame
+
+  def pop_frame(self):
+    self.frames.pop():
+    if self.frames:
+      self.frame = slef.frames[-1]
+    else:
+      self.frame = None
+
+  def run_frame(self):
+    pass
